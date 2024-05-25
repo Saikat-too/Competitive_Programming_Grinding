2c988580171d241f4d94e8d9dd47438b866118b9
Saikat-too <singhasaikat37@gmail.com>
2024-05-25 21:32:38 +0600

    ac

--- Codeforces/CR947/a.cpp
+++ Codeforces/CR947/a.cpp
@@ -1,6 +1,6 @@
 /*
   Saikat Singha 
-  25.05.2024
+  24.05.2024
 
 */
 
@@ -13,7 +13,34 @@ i64 MOD = 1e9+7;
 
 void solve(){
 
-
+  int n;
+  cin >> n ;
+  vector<int>a(n);
+  for(int i = 0;i<n;i++) cin >> a[i];
+  int ind = -1;
+  for(int i = 1;i<n;i++){
+    if(a[i]>=a[i-1]) continue;
+    ind = i;
+    break;
+  }
+  vector<int>ans ; 
+  if(ind==-1){
+    cout << "YES" << '\n';
+    return ;
+  }
+  
+  for(int i = ind;i<n;i++)ans.push_back(a[i]);
+  for(int i = 0;i<ind;i++)ans.push_back(a[i]);
+  sort(a.begin(),a.end());
+  for(int i = 0;i<n;i++){
+    if(a[i]!=ans[i]){
+      cout << "NO" << '\n';
+      return ;
+    }
+  }
+  
+  cout << "YES" << '\n';
+  return ;
 }
 
 int main(){
