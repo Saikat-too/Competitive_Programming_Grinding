ca733eb2604a7df32b54d38ef60b548809d5b85d
Saikat-too <singhasaikat37@gmail.com>
2024-06-30 14:30:40 +0600

    gitignore file

---
+++ Codeforces/CR900/c.cpp
@@ -0,0 +1,26 @@
+/*
+  Saikat Singha 
+  30.06.2024
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64 = long long ;
+
+i64 MOD = 1e9+7;
+
+void solve(){
+    
+
+}
+
+int main(){
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--) solve();
+
+}
