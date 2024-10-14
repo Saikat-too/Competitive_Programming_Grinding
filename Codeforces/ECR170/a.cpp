85f54e443c7691f15de838e2d7ed8d25e04f2599
Saikat-too <saikatsingha00q@gmail.com>
2024-10-14 22:03:25 +0600

    Solve 2 problems

--- Codeforces/ECR170/a.cpp
+++ Codeforces/ECR170/a.cpp
@@ -4,7 +4,27 @@ using namespace std;
 
 #define ll long long
 
-void solve() {}
+void solve() {
+
+  string s, t;
+  cin >> s >> t;
+
+  int ind = 0;
+
+  for (int i = 0; i < (int)s.length(); i++) {
+    if (s[i] == t[i])
+      ind++;
+    else
+      break;
+  }
+
+  int ans = (int)s.length() + ((int)t.length() - ind);
+  if (ind != 0)
+    ans += 1;
+  cout << ans << '\n';
+
+  return;
+}
 
 int main() {
 
--- Codeforces/ECR170/b.cpp
+++ Codeforces/ECR170/b.cpp
@@ -4,13 +4,31 @@ using namespace std;
 
 #define ll long long
 
-void solve() {}
+ll mod = 1e9 + 7;
 
 int main() {
 
   int t;
   cin >> t;
+  vector<ll> a(100001, 1);
+  a[1] = 2;
 
-  while (t--)
-    solve();
+  for (int i = 2; i <= 100001; i += 1) {
+    a[i] = a[i - 1] * 2 % mod;
+  }
+
+  vector<ll> v1, v2;
+  for (int i = 0; i < t; i++) {
+    int x;
+    cin >> x;
+    v1.push_back(x);
+  }
+
+  for (int i = 0; i < t; i++) {
+    int x;
+    cin >> x;
+    cout << a[x] << '\n';
+  }
+
+  return 0;
 }
