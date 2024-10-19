2cc1015fbecd7df9b48ed39f2f0ef35b7cb62ecf
Saikat-too <saikatsingha00q@gmail.com>
2024-10-19 17:47:51 +0600

    AC

---
+++ CSES/IntroductoryProblems/trailing_zeros.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+  ll n;
+  cin >> n;
+  int ans = 0;
+  for (ll i = 5; i <= n; i *= 5) {
+    ans += n / i;
+  }
+
+  cout << ans << '\n';
+  return 0;
+}
