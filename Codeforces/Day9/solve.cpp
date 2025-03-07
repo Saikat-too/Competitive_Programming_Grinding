e1aad8f6a21ff100519b9fdfa6158374f6ef3091
Saikat-too <saikatsingha00q@gmail.com>
2025-03-07 08:27:44 +0600

    jus t a day

---
+++ Codeforces/Day9/solve.cpp
@@ -0,0 +1,21 @@
+/*
+    Saikat Singha
+    03.03.2025
+*/
+
+#include<bits/stdc++.h>
+using namespace std ;
+using ll = long long ;
+
+void solve(){
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    cin >> t ;
+    while(t--) solve();
+    return 0 ;
+}
